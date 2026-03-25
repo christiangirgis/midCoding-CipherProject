758fbe139ac58e7f8da904e2bd1fd71c94d5acd3
christiangirgis <christian.girgis.stu@cedarpark.org>
2026-03-25 12:55:20 -0700

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,9 @@
-ord('A')
+message = input("Enter your message: ")
+shift = int(input("Enter the shift number: "))
+encoded = ""
+for char in message:
+    chartoEncode = ord(char)
+    shiftedChar = chartoEncode + shift
+    encodedChar = chr(shiftedChar)
+    encoded += encodedChar
+print(encoded)
