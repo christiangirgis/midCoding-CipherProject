e61125c39d05bb9166f04cbdaa42e0ae3eed5c94
christiangirgis <christian.girgis.stu@cedarpark.org>
2026-03-31 12:28:27 -0700

    decode

--- cipher.py
+++ cipher.py
@@ -7,3 +7,16 @@ for char in message:
     encodedChar = chr(shiftedChar)
     encoded += encodedChar
 print(encoded)
+
+
+
+
+message = input("Enter your message: ")
+shift = int(input("Enter the shift number: "))
+encoded = ""
+for char in message:
+    chartoEncode = ord(char)
+    shiftedChar = chartoEncode - shift
+    encodedChar = chr(shiftedChar)
+    encoded += encodedChar
+print(encoded)
