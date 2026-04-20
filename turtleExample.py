059a4e6d3f950ff9bfa54134d3f71ceaa9ab645b
christiangirgis <christian.girgis.stu@cedarpark.org>
2026-04-20 12:19:50 -0700

    Create turtleExample.py

---
+++ turtleExample.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed (700)
+for i in range(41):
+    t.forward(67)
+    t.right(41)
+    t.right(67)
+    t.right(41)
+    t.right(67)
+    t.right(41)
+    t.right(20)
+    t.right(100)
+    t.right(3)
+    t.right(27)
+    t.right(71)
+    t.right(1818181)
+turtle.done()
